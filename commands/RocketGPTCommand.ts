1e34bf381155ea334c89d8143361045a69c2d8d2
John Walz <johnwalz97@gmail.com>
2023-04-18 17:31:38 -0400

    getting basic ai fucntion to work

--- commands/RocketGPTCommand.ts
+++ commands/RocketGPTCommand.ts
@@ -1,18 +1,24 @@
 import {
     IHttp,
+    IMessageBuilder,
     IModify,
+    IModifyCreator,
+    IPersistence,
     IRead,
 } from "@rocket.chat/apps-engine/definition/accessors";
+import { IMessage } from "@rocket.chat/apps-engine/definition/messages";
+import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
 import {
     ISlashCommand,
     SlashCommandContext,
 } from "@rocket.chat/apps-engine/definition/slashcommands";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
 import { RocketGptApp } from "../RocketGptApp";
 
 export class RocketGPTCommand implements ISlashCommand {
     public command = "rocketgpt";
-    public i18nParamsExample = "RocketGPTCommand_Params";
-    public i18nDescription = "RocketGPTCommand_Description";
+    public i18nParamsExample = "Say anything...";
+    public i18nDescription = "Talk to RocketGPT";
     public providesPreview = false;
 
     constructor(private readonly app: RocketGptApp) {}
@@ -23,21 +29,19 @@ export class RocketGPTCommand implements ISlashCommand {
         modify: IModify,
         http: IHttp
     ): Promise<void> {
-        const sender = context.getSender();
-        const room = context.getRoom();
+        const creator: IModifyCreator = modify.getCreator();
+        const sender: IUser = (await read.getUserReader().getAppUser()) as IUser;
+        const room: IRoom = context.getRoom();
         const args = context.getArguments().join(" ");
 
         if (!args) {
-            await modify
-                .getNotifier()
-                .notifyUser(
-                    sender,
-                    modify
-                        .getCreator()
-                        .startMessage()
-                        .setText("Please provide a message to send to ChatGPT.")
-                        .getMessage()
-                );
+            const messageTemplate: IMessage = {
+                text: "Please provide a message to send to ChatGPT.",
+                sender,
+                room
+            };
+            const messageBuilder: IMessageBuilder = creator.startMessage(messageTemplate);
+            await creator.finish(messageBuilder)
             return;
         }
 
@@ -45,8 +49,7 @@ export class RocketGPTCommand implements ISlashCommand {
             .getEnvironmentReader()
             .getSettings()
             .getValueById("chatgpt_api_key");
-        const url =
-            "https://api.openai.com/v1/engines/davinci-codex/completions";
+        const url = "https://api.openai.com/v1/chat/completions";
 
         const headers = {
             "Content-Type": "application/json",
@@ -54,39 +57,41 @@ export class RocketGPTCommand implements ISlashCommand {
         };
 
         const payload = {
-            prompt: args,
-            max_tokens: 50,
-            n: 1,
-            stop: null,
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: args }],
             temperature: 0.8,
         };
 
         try {
             const response = await http.post(url, { headers, data: payload });
-            const responseData = JSON.parse(response.content || "{}");
-            const message = responseData.choices[0].text.trim();
 
-            await modify
-                .getCreator()
-                .startMessage()
-                .setSender(sender)
-                .setRoom(room)
-                .setText(message)
-                .getMessage();
+            if (response.statusCode !== 200) {
+                const messageTemplate: IMessage = {
+                    text: "Error processing the request. Please try again later. Status code: " + response.statusCode,
+                    sender,
+                    room
+                };
+                const messageBuilder: IMessageBuilder = creator.startMessage(messageTemplate);
+                await creator.finish(messageBuilder)
+                return;
+            }
+
+            const responseData = JSON.parse(response.content || "{}");
+            const message = responseData.choices[0].message.content.trim();
+
+            const messageTemplate: IMessage = { text: message, sender, room };
+            const messageBuilder: IMessageBuilder = creator.startMessage(messageTemplate);
+            await creator.finish(messageBuilder)
         } catch (error) {
             this.app.getLogger().error("Error calling ChatGPT:", error);
-            await modify
-                .getNotifier()
-                .notifyUser(
-                    sender,
-                    modify
-                        .getCreator()
-                        .startMessage()
-                        .setText(
-                            "Error processing the request. Please try again later."
-                        )
-                        .getMessage()
-                );
+
+            const messageTemplate: IMessage = {
+                text: "Error processing the request. Please try again later." + error,
+                sender,
+                room
+            };
+            const messageBuilder: IMessageBuilder = creator.startMessage(messageTemplate);
+            await creator.finish(messageBuilder)
         }
     }
 }
